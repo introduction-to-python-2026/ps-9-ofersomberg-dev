d616642dc59414eb75be0396f6b3f75c84a97efa
ofersomberg-dev <ofersomberg@gmail.com>
2026-01-02 14:28:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+import pandas as pd
+df = pd.read_csv('/content/parkinsons.csv')
+df = df.dropna()
 
+import seaborn as sns
+sns.pairplot(df, vars=['MDVP:Jitter(%)', 'MDVP:Shimmer'],hue='status')
+selected_features = ['MDVP:Jitter(%)', 'MDVP:Shimmer']
+x = df[selected_features]
+y = df['status']
+
+from sklearn.preprocessing import MinMaxScaler
+
+scaler = MinMaxScaler()
+x = scaler.fit_transform(x)
+
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+from sklearn.neighbors import KNeighborsClassifier
+
+model = KNeighborsClassifier(n_neighbors=10)
+model.fit(x_train, y_train)
+
+from sklearn.metrics import accuracy_score
+
+y_pred = model.predict(x_test)
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {accuracy}')
