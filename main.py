ca2f29402652d18775c0ea782505ea25b509bdd8
ofersomberg-dev <ofersomberg@gmail.com>
2026-01-07 07:58:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,29 +2,26 @@ import pandas as pd
 import joblib
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
-from sklearn.tree import DecisionTreeClassifier
-# Load data
-df = pd.read_csv('parkinsons.csv')
-df = df.dropna()
-print(df.columns.to_list())
 
-selected_features = ['RPDE','PPE']
+
+df = pd.read_csv('parkinsons.csv')
+selected_features = ['MDVP:Fo()Hz','PPE']
 X = df[selected_fetures]
 y = df['status']
 
 scaler = MinMaxScaler()
 X_scaled = scaler.fit_transform(X)
 
-X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, random_state=42)
 
-DTC = DecisionTreeClassifier(max_depth = 3)
-DTC.fit(X_train, y_train)
+model = KNeighborsClassifier(n_neighbors=10)
+model.fit(X_train, y_train)
 
-y_pred = DTC.predict(X_test)
+y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
-print(accuracy)
+print(f'Accuracy:{accuracy}')
 
 # Save model
 joblib.dump(model, 'my_model.joblib')
