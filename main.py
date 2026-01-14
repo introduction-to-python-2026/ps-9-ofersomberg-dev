adb01e26e1aa8af0669b507a5eab55d542b48be3
ofersomberg-dev <ofersomberg@gmail.com>
2026-01-14 07:08:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,23 +5,31 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 
-
+# Load dataset
 df = pd.read_csv('parkinsons.csv')
-selected_features = ['MDVP:Fo()Hz','PPE']
+
+# Select features and label
+selected_features = ['MDVP:Fo()Hz', 'PPE']
 X = df[selected_features]
 y = df['status']
 
+# Scale data
 scaler = MinMaxScaler()
 X_scaled = scaler.fit_transform(X)
 
-X_train, X_test, y_train, y_test = train_test_split( X_scaled, y, test_size=0.2, random_state=42)
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(
+    X_scaled, y, test_size=0.2, random_state=42
+)
 
+# Train model
 model = KNeighborsClassifier(n_neighbors=10)
 model.fit(X_train, y_train)
 
+# Evaluate
 y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
-print(f'Accuracy:{accuracy}')
+print(f"Accuracy: {accuracy}")
 
 # Save model
 joblib.dump(model, 'my_model.joblib')
